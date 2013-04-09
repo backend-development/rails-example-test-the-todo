f531d4d208ead1d2c2a29544c53ea0690679b576
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-04-09 21:36:34 +0200

    emptly rails with rspec, capybara, poltergeist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '0cd5271a5140033768f00640f0ac254bb2393105e7aa7fb28ecd8576c989304c91c23b46819ac4c13e41ff770eedcbe6c9659bbe29ec179a2764618a12851b99'
